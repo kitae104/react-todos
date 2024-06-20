a1c7d71c7a14e7a0ae59fffefcf56617a6cffec9
Kitae's Github <aqua0405@gmail.com>
2024-06-20 11:27:10 +0900

    240620-1127

---
+++ src/components/TodoRowItem.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function TodoRowItem(props: { rowNumber: number, rowDescription: string, rowAssigned: string, deleteTodo: Function }) {
+
+    return (
+        <tr onClick={() => props.deleteTodo(props.rowNumber)}>
+            <th scope="row">{props.rowNumber}</th>
+            <td>{props.rowDescription}</td>
+            <td>{props.rowAssigned}</td>
+        </tr>
+    )
+}
+
+export default TodoRowItem
